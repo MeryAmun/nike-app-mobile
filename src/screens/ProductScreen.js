d6219308308de243a08a2b050721bbc0acc16f78
Chebs Awah <chebesiawah@gmail.com>
2023-08-10 12:40:27 +0100

    playground set and products screen done

---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { StyleSheet, View, Image, FlatList } from "react-native";
+import products from '../data/products';
+
+const ProductScreen = () => (
+    <FlatList
+        data={products}
+        renderItem={({ item }) => (
+         <View style={styles.itemContainer}>
+           <Image source={{ uri: item.image }} style={styles.image} />
+         </View>
+          )}
+          numColumns={2}
+      />
+  )
+
+
+export default ProductScreen
+
+
+const styles = StyleSheet.create({
+    image: {
+      width: "100%",
+      aspectRatio: 1,
+    },
+    itemContainer:{
+      width:"50%",
+      padding:1
+    }
+  });
